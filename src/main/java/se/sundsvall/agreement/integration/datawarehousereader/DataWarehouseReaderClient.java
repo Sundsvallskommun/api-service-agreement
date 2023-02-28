38c92cf0d42dd24de14f98ca7b5a93b9b9719d16
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-02-28 11:24:03 +0100

    UF-4484: Adjusted circuit breaker settings
    - Removed circuitbreaker annotation on feign client

--- src/main/java/se/sundsvall/agreement/integration/datawarehousereader/DataWarehouseReaderClient.java
+++ src/main/java/se/sundsvall/agreement/integration/datawarehousereader/DataWarehouseReaderClient.java
@@ -11,11 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import generated.se.sundsvall.datawarehousereader.AgreementResponse;
 import generated.se.sundsvall.datawarehousereader.Category;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import se.sundsvall.agreement.integration.datawarehousereader.configuration.DataWarehouseReaderConfiguration;
 
 @FeignClient(name = CLIENT_REGISTRATION_ID, url = "${integration.datawarehousereader.url}", configuration = DataWarehouseReaderConfiguration.class)
-@CircuitBreaker(name = CLIENT_REGISTRATION_ID)
 public interface DataWarehouseReaderClient {
 
 	@GetMapping(path = "agreements", produces = APPLICATION_JSON_VALUE)
