0ad41d99a785de6b2eaf94b473e9ba69a2cff64c
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2023-10-13 11:18:31 +0200

    UF-5903: Add useReturnTypeSchema = true

--- src/main/java/se/sundsvall/agreement/api/PagedAgreementResource.java
+++ src/main/java/se/sundsvall/agreement/api/PagedAgreementResource.java
@@ -40,7 +40,7 @@ public class PagedAgreementResource {
 
 	@GetMapping(path = "/{partyId}", produces = { APPLICATION_JSON_VALUE, APPLICATION_PROBLEM_JSON_VALUE })
 	@Operation(summary = "Get agreements connected to a party-ID, optionally filtered by provided categories")
-	@ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = AgreementResponse.class)))
+	@ApiResponse(responseCode = "200", description = "Successful operation", useReturnTypeSchema = true)
 	@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(oneOf = { Problem.class, ConstraintViolationProblem.class })))
 	@ApiResponse(responseCode = "500", description = "Internal Server error", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
 	@ApiResponse(responseCode = "502", description = "Bad Gateway", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
